6d80ed3821120481a26eb1694602ef1197a21a83
nnismithllc <65423204+nnismithllc@users.noreply.github.com>
2020-08-14 19:07:51 -0400

    updated function init

--- Develop/index.js
+++ Develop/index.js
@@ -1,94 +1,86 @@
-var inquirer = require("inquirer");
-var fs = require('fs');
-var readMe =fs.readFileSync ('readMe.text',"utf-8" );
+const inquirer = require("inquirer");
+const fs = require('fs');
+const util = require("util");
 
-// array of questions for user
-inquirer.prompt([
-{
+const writeFileAsync = util.promisify(fs.writeFile);
+
+function promptUser() {
+ return inquirer.prompt ([
+  {
     type: "input",
-    name: "project",
-    message: "What is the name of this project?"
+    message: "What is the name of this project?",
+    name: "project"
+   
+  },
+  {
+    type: "input",
+    message: "Please describe the functionality of the project in full detail.",
+    name: "description"
     
   },
   {
     type: "input",
-    name: "description",
-    message: "Please describe the functionality of the project in full detail."
+    message: "Please provide a Table of Contents here for your Project README file.",
+    name: "contents"
+  },
+  {
+    type: "input",
+    message: "What are the steps and programs required to run your project? Please provide step by step instructions?",
+    name: "installation"
     
   },
   {
     type: "input",
-    name: "contents",
-    message: "Please provide a Table of Contents here for your Project README file."
-  },
-  {
-    type: "input",
-    name: "installation",
-    message: "What are the steps and programs required to run your project? Please provide step by step instructions?"
-    
-  },
-  {
-    type: "input",
-    name: "usage",
-    message: "What can the user use this project for? Please explain."
+    message: "What can the user use this project for? Please explain.",
+    name: "usage"
   },
   {
     type: "list",
-    name: "licensing",
     message: "What licensing would you like to use for this project?",
     choices: [
         `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`,
          `[![License: Artistic-2.0](https://img.shields.io/badge/License-Perl-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)`,
           `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`,
-           `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`]
+           `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`],
+    name: "licensing"
 },   
 {
   type: "input",
-  name: "credits",
-  message: "Who did you collaborate with on this project?"
+  message: "Who did you collaborate with on this project?",
+  name: "credits"
 },
 {
   type: "input",
-  name: "test",
-  message: "Have you tested your Project? Please run test."
+  message: "Have you tested your Project? Please run test.",
+  name: "test"
 },
 {
   type: "checkbox",
-  name: "badge",
   message: "Do you have a merit badge?",
   choices: [
     "Yes", 
     "No"
-  ]
+  ],
+  name: "badge"
 },
 {
   type: "input",
-  name: "questions",
-  message: "Do you have any questions?"
+  message: "Do you have any questions?",
+  name: "questions"
 },
 
 {
   //adds email to the "questions" section 
 type: "input",
-name: "email",
-message: "I can be reached at."
+message: "I can be reached at.",
+name: "email"
 },
 
+ ])
 
-]) .then(function(response) {
-  const title = response.project;
-  const description = response.description;
-  const contents = response.contents;
-  const installation = response.installation;
-  const usage = response.usage;
-  const license = response.license;
-  const badge = response.badge;
-  const test = response.tests;
-  const credits = response.credits;
-  const questions = response.questions;
-  const email = response.email;
-  const markDown = 
- `## ${title}
+function generateMarkdown(answers) {
+  return `
+## ${title}
   
 ${license}
   
@@ -125,28 +117,29 @@ ${badge}
   
 ## Testing 
   
-${test}`; 
+${test}`
+
 }
 
-// function to write README file
-fs.writeFile("README.md", markDown, function(err) {
-      
-  if (err) {
-    return console.log(err);
-  } else {
-  console.log("Success!");
+// function writeToFile (README, data){}
+
+function init () {
+    inquirer
+    .prompt(questions)
+  .then ((userAnswers) => {
+    const readme = generateMarkdown(UserAnswers);
+    fs.writeFile("ReadME.md", readme, function (err) {
+if (err) {
+    return console.log (err);
+    
 }
-
-
+    console.log("Successfully wrote to README.md");
+  });
+  console.log(userAnswers);
 })
+  .catch ((err) => console.log (err));
+    console.log(err);
+  }
 
-})
-
-
-// function to initialize program
-function init() {
-
+  init ();
 }
-
-// function call to initialize program
-init();
